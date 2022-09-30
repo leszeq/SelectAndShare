56fd10b7f2b5fecfd38a69a97387ac05d7027596
leszeq <37637730+leszeq@users.noreply.github.com>
2022-09-30 11:59:18 +0200

    adding axios and geocoding

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,31 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyASkBUGtaNYMic9OuRboLFlRsOqrrk8m8o';
+
+type GoogleGeocodingResponse = {
+	results: { geometry: { location: { lat: number; lng: number } } };
+};
+
 function searchAddressHandler(event: Event) {
 	event.preventDefault();
-    // const enteredAddress = addressInput.value;
+	const enteredAddress = addressInput.value;
 
-    // send this to Google's API
-
-
+	axios
+		.get<GoogleGeocodingResponse>(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+				enteredAddress
+			)}&key=${GOOGLE_API_KEY}`
+		)
+		.then((response) => {
+			console.log(response);
+			// const coordinates = response.data.results[0].geometry.location;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
 
 form.addEventListener('submit', searchAddressHandler);
